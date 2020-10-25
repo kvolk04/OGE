eb03147d0920aa4d2b57c5647ebe39ff947be937
kvolk04 <54364722+kvolk04@users.noreply.github.com>
2020-10-25 19:30:29 +0300

    123

---
+++ ege26/27424.pas
@@ -0,0 +1,18 @@
+﻿var
+Max, i, s, N, a, b, Min, MinRaz: integer;
+begin
+  s:=0;
+  MinRaz:=10000;
+  readln(n);
+  for i := 1 to N do begin
+    readln(a,b);
+    if a>b then begin Max:=a; Min:=b end
+    else begin Max:=b; Min:=a end;
+    s:=s+Max;
+    if ((Max-Min) mod 3 <> 0) and ( Max - Min < MinRaz)
+    then MinRaz:= Max - Min
+  end;
+  if s mod 3 = 0 then 
+    writeln(s-MinRaz)
+    else writeln(s);
+end.
---
+++ ege26/27891.pas
@@ -0,0 +1,23 @@
+﻿begin
+  var n := 0;
+  read(n);
+  var max2 := 0;
+  var max7 := 0;
+  var max14 := 0;
+  var max0 := 0;
+  for var i := 1 to n do
+  begin
+    var x: integer;
+    read(x);
+    if (x mod 2 = 0) and (x mod 7 <> 0) and (x > max2) then max2 := x;
+    if (x mod 7 = 0) and (x mod 2 <> 0) and (x > max7) then max7 := x;
+    if (x mod 2 = 0) and (x mod 7 = 0) and (x > max14) then max14 := x;
+    if (x mod 2 <> 0) and (x mod 7 <> 0) and (x > max0) then max0 := x;
+  end;
+  var prods: array[1..4] of integer := (max2 * max7, max14 * max2, max14 * max7, max14 * max0);
+  var max := prods[1];
+  for var i := 2 to 4 do
+    if prods[i] > max then max := prods[i];
+    
+  write(max);
+end.
